d1a55b7da879a7ccdb9f7affedbcfb50a2b42e23
Jose Torreblanca <josewhitetower@gmail.com>
2019-10-16 19:36:13 +0200

    add navbar and font

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox'
-import './Palette.css'
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
+import NavBar from "./NavBar";
 
 export default class Palette extends Component {
-    state = {
-        level:500
-    }
+  state = {
+    level: 500
+  };
 
-    changeLevel = (level) => this.setState({level})
-    render() {
-        const colorBoxes = this.props.palette.colors[this.state.level].map(color => {
-            return <ColorBox background={color.hex} name={color.name}/>
-        })
-        return (
-            <div className="Palette">
-                <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-            </div>
-        )
-    }
+  changeLevel = level => this.setState({ level });
+  render() {
+    const colorBoxes = this.props.palette.colors[this.state.level].map(
+      color => {
+        return <ColorBox background={color.hex} name={color.name} />;
+      }
+    );
+    return (
+      <div className="Palette">
+        <NavBar changeLevel={this.changeLevel} level={this.state.level} />
+        <div className="Palette-colors">{colorBoxes}</div>
+      </div>
+    );
+  }
 }
