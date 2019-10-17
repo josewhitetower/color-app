6523cab1b965cb076588d7aff9aa92aa058f74bb
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2019-10-17 15:17:46 +0200

    Added sekect and material ui

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -5,17 +5,21 @@ import './Palette.css'
 
 export default class Palette extends Component {
     state = {
-        level:500
+        level:500,
+        format: 'hex'
     }
 
     changeLevel = (level) => this.setState({level})
+
+    changeFormat = (format) => this.setState({format})
+
     render() {
         const colorBoxes = this.props.palette.colors[this.state.level].map(color => {
-            return <ColorBox background={color.hex} name={color.name}/>
+            return <ColorBox background={color[this.state.format]} name={color.name}/>
         })
         return (
             <div className="Palette">
-                <Navbar level={this.props.level} changeLevel={this.changeLevel} />
+                <Navbar level={this.props.level} changeLevel={this.changeLevel}  changeFormat={this.changeFormat} format={this.state.format}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
