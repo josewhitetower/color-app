ddd70cc3ca3417db8025f370c3447aeae426f907
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2019-10-17 16:13:25 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import Palette from './components/Palette';
 import seedColors from './seedColors'
 import {generatePalette} from './helpers/colorHelpers'
@@ -7,7 +8,12 @@ function App() {
 
   return (
     <div className="App">
-      <Palette palette={generatePalette(seedColors[5])}/>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Main Route</h1>}/>
+        <Route exact path="/palette/:id" render={()=><h1>Palette Route</h1>}/>
+      </Switch>
+
+      {/* <Palette palette={generatePalette(seedColors[5])}/> */}
     </div>
   );
 }
