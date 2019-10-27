18f8f0a1694ece256d134a5cb7c740b54c3c5f27
Jose Torreblanca <josewhitetower@gmail.com>
2019-10-27 19:48:08 +0100

    start to style palte form nav

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -20,23 +20,6 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: "flex"
   },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
   hide: {
     display: "none"
   },
@@ -132,7 +115,6 @@ export default function PersistentDrawerLeft({
   return (
     <div className={classes.root}>
       <PaletteFormNav
-        classes={classes}
         open={open}
         handleDrawerOpen={handleDrawerOpen}
         handleSubmit={handleSubmit}
--- src/components/PaletteFormNav.js
+++ src/components/PaletteFormNav.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import clsx from "clsx";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -7,84 +7,110 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+const drawerWidth = 400;
+const useStyles = makeStyles(theme => ({
+  roote: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: "64px"
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  }
+}));
 
-export default class PaletteFormNav extends Component {
-  state = {
-    newPaletteName: ""
-  };
-  componentDidMount() {
-    const { palettes } = this.props;
+function PaletteFormNav({
+  palettes,
+  open,
+  handleDrawerOpen,
+  handleSubmit,
+  history
+}) {
+  const [newPaletteName, setNewPaletteName] = React.useState("");
+
+  React.useEffect(() => {
     ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
       palettes.every(
         ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
       )
     );
-  }
-  handleOnPaletteNameChange = e => {
-    this.setState({ newPaletteName: e.target.value });
+  });
+  const classes = useStyles();
+
+  const handleOnPaletteNameChange = e => {
+    setNewPaletteName(e.target.value);
   };
 
-  render() {
-    const {
-      classes,
-      open,
-      handleDrawerOpen,
-      handleSubmit,
-      history
-    } = this.props;
-    return (
-      <div>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          color="default"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open
-          })}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              Persistent drawer
-            </Typography>
-            <ValidatorForm
-              onSubmit={() => handleSubmit(this.state.newPaletteName)}
-            >
-              <TextValidator
-                value={this.state.newPaletteName}
-                label="Palette Name"
-                onChange={this.handleOnPaletteNameChange}
-                validators={["required", "isPaletteNameUnique"]}
-                errorMessages={[
-                  "Enter Palette Name",
-                  "Palette Name must be unique"
-                ]}
-              />
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="default"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+        <div className={classes.navBtns}>
+          <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
+            <TextValidator
+              value={newPaletteName}
+              label="Palette Name"
+              onChange={handleOnPaletteNameChange}
+              validators={["required", "isPaletteNameUnique"]}
+              errorMessages={[
+                "Enter Palette Name",
+                "Palette Name must be unique"
+              ]}
+            />
 
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => history.goBack()}
-              >
-                Go Back
-              </Button>
-              <Button variant="contained" color="primary" type="submit">
-                Save Palette
-              </Button>
-            </ValidatorForm>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+            <Button variant="contained" color="primary" type="submit">
+              Save Palette
+            </Button>
+          </ValidatorForm>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => history.goBack()}
+          >
+            Go Back
+          </Button>
+        </div>
+      </AppBar>
+    </div>
+  );
 }
+
+export default PaletteFormNav;
