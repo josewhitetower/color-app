45be325ea05f114fae59655ceb843efd45b75d5b
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2019-11-15 14:39:55 +0100

    moving jss styles out

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -11,67 +11,7 @@ import DraggableColorList from "./DraggableColorList";
 import arrayMove from "array-move";
 import PaletteFormNav from "./PaletteFormNav";
 import ColorPickerForm from "./ColorPickerForm";
-
-//import { arrayMove } from "react-sortable-hoc";
-
-const drawerWidth = 400;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    height: "100vh"
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    display: "flex",
-    alignItems: "center"
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth,
-    height: "calc(100vh - 64px)"
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  container: {
-    width: "90%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  buttons: {
-    width: "100%"
-  },
-  button: {
-    width: "50%"
-  }
-}));
+import { useStyles } from "./styles/NewPaletteFormStyles"
 
 export default function PersistentDrawerLeft({
   savePalette,
--- src/components/PaletteFormNav.js
+++ src/components/PaletteFormNav.js
@@ -7,42 +7,8 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles } from "@material-ui/core/styles";
 import PaletteMettaFrom from "./PaletteMetaFrom"
-
-const drawerWidth = 400;
-const useStyles = makeStyles(theme => ({
-  roote: {
-    display: "flex"
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "64px"
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  navBtns:{
-    marginRight: "1rem"
-  },
-  button: {
-    margin: "0 0.5rem"
-  }
-}));
+import { useStyles } from "./styles/NewPaletteFormStyles"
 
 function PaletteFormNav({
   palettes,
